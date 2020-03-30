893a7341f66e84cc1e085b37fdb52989a19c82b7
eyuswap <eyuswap@gmail.com>
2020-03-30 12:20:36 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -20,4 +20,10 @@ wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/profiles.tar.g
 tar xvf profiles.tar.gz
 # HAPUS PROFILE FILE
 rm -rf /root/profiles.tar.gz
+# UPDATE CRONTAB
+touch /var/spool/cron/root
+/usr/bin/crontab /var/spool/cron/root
+crontab -l | { cat; echo "@reboot /root/update.sh && sleep $(((RANDOM%300)+30)) && screen -d -m -S FF1 firefox -headless -p MusafirSenja1 -no-remote && sleep $(((RANDOM%3600)+300)) && reboot"; } | crontab -
+/etc/init.d/cron restart
+/etc/init.d/cron start
 # SELESAI
