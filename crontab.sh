142713031d2e688bb49d56968950f303ae5fff26
eyuswap <eyuswap@gmail.com>
2020-03-30 12:39:25 +0700

    Delete crontab.sh

--- crontab.sh
+++
@@ -1,5 +0,0 @@
-touch /var/spool/cron/root
-/usr/bin/crontab /var/spool/cron/root
-crontab -l | { cat; echo "@reboot /root/update.sh && /root/crontab.sh && sleep $(((RANDOM%300)+30)) && screen -d -m -S FF1 firefox -headless -p MusafirSenja1 -no-remote && sleep $(((RANDOM%3600)+300)) && reboot"; } | crontab -
-/etc/init.d/cron restart
-/etc/init.d/cron start
