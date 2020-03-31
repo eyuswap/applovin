2e21add9a791f007c9a58e959d0ac67abde30c11
eyuswap <eyuswap@gmail.com>
2020-04-01 02:26:30 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # UPDATE UBUNTU
-sudo apt update
+sudo apt-get update -y
 # MENGHAPUS DATA
 rm -rf /opt/firefox
 rm -rf /root/virtualhost.sh
@@ -16,7 +16,7 @@ chmod +x /root/virtualhost.sh
 wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/hosts -O /etc/hosts
 wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/index.php -O /var/www/applovin.com/index.php
 wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/proxy.txt -O /var/www/applovin.com/proxy.txt
-wget -q https://eyus.co.id/applovin/firefox.tar.bz2 -O /opt/firefox.tar.bz2
+wget -q https://ftp.mozilla.org/pub/firefox/releases/56.0/linux-x86_64/en-US/firefox-56.0.tar.bz2 -O /opt/firefox.tar.bz2
 wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/profiles.tar.gz -O /root/profiles.tar.gz
 # EXTRACT DATA PROFILE
 tar xvf profiles.tar.gz
@@ -25,11 +25,13 @@ rm -rf /root/profiles.tar.gz
 cd /opt
 tar -jxvf firefox.tar.bz2
 rm -rf /opt/firefox.tar.bz2
+sudo mv /usr/bin/firefox /usr/bin/firefox-backup
+sudo ln -s /opt/firefox/firefox /usr/bin/firefox
 cd /root
 # UPDATE CRONTAB
 touch /var/spool/cron/root
 /usr/bin/crontab /var/spool/cron/root
-crontab -l | { cat; echo "@reboot /root/update.sh && sleep $(((RANDOM%4200)+5)) && screen -d -m -S FF1 /opt/firefox/firefox -headless && sleep $(((RANDOM%3600)+300)) && /sbin/shutdown -r now"; } | crontab -
+crontab -l | { cat; echo "@reboot /root/update.sh && sleep $(((RANDOM%4200)+5)) && screen -d -m -S FF firefox -headless && sleep $(((RANDOM%3600)+300)) && /sbin/shutdown -r now"; } | crontab -
 /etc/init.d/cron restart
 /etc/init.d/cron start
 # SELESAI
