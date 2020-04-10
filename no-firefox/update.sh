a826d60bc58db3d5790da8b6ecfbdf4886e205e5
eyuswap <eyuswap@gmail.com>
2020-04-11 01:56:28 +0800

    Update update.sh

--- no-firefox/update.sh
+++ no-firefox/update.sh
@@ -12,7 +12,7 @@ chmod +x /root/virtualhost.sh
 /root/virtualhost.sh
 # DOWNLOAD HOST, SCRIPT PHP, DAN PROFILE FIREFOX
 wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/hosts -O /etc/hosts
-wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/index.php -O /var/www/applovin.com/index.php
+wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/no-firefox/index.php -O /var/www/applovin.com/index.php
 # UPDATE CRONTAB
 touch /var/spool/cron/root
 /usr/bin/crontab /var/spool/cron/root
