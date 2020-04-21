3b024393b485d4462c3b4e60009561a7e5bd1a32
eyuswap <eyuswap@gmail.com>
2020-04-21 21:29:34 +0800

    Update index.php

--- no-firefox/index.php
+++ no-firefox/index.php
@@ -20,12 +20,12 @@ $userAgent[] = "Mozilla/5.0 (iPad; CPU OS 13_4_1 like Mac OS X) AppleWebKit/605.
 $userAgent[] = "Mozilla/5.0 (iPad; CPU OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1";
 $UARand = array_rand($userAgent);return $userAgent[$UARand];}
 function DataClick() {
-$time = 60*30;
+$time = 60*60;
 srand(floor(time() / $time)); 
-$arrClick = array("(1&p|2&p|3&p)","(1&p|2&p|3&p|4&p)","(1&p|2&p|3&p|4&p|5&p)","(1&p|2&p|3&p|4&p|5&p|6&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p|0&p|a&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p|0&p|a&p|b&p)","(a&p|2&p|3&p)","(b&p|2&p|3&p|4&p)","(c&p|2&p|3&p|4&p|5&p)","(d&p|2&p|3&p|4&p|5&p|6&p)","(e&p|2&p|3&p|4&p|5&p|6&p|7&p)","(f&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p)","(a&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p)","(b&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p|0&p)","(c&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p|0&p&1)","(a&p|b&p|c&p|d&p|e&p|f&p)","(a&p|b&p|c&p|d&p|e&p|f&p|1&p)","(a&p|b&p|c&p|d&p|e&p|f&p|1&p|2&p)","(a&p|b&p|c&p|d&p|e&p|f&p|1&p|2&p|3&p)","(a&p|c&p|d&p|e&p|f&p|1&p|2&p|3&p|9&p)","(a&p|b&p|c&p|d&p|e&p|1&p|2&p|3&p|9&p|8&p)","(b&p|c&p|d&p|e&p|f&p|1&p|3&p|9&p|8&p|7&p)","(a&p|b&p|d&p|e&p|f&p|1&p|2&p|8&p|7&p|6&p)","(a&p|d&p|f&p|1&p)","(a&p|2&p|f&p|4&p|c&p)","(1&p|7&p|3&p)","(1&p|2&p|3&p|d&p|)");
+$arrClick = array("(1&p|2&p|3&p)","(1&p|2&p|3&p|4&p)","(1&p|2&p|3&p|4&p|5&p)","(1&p|2&p|3&p|4&p|5&p|6&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p|0&p|a&p)","(1&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p|0&p|a&p|b&p)","(a&p|2&p|3&p)","(b&p|2&p|3&p|4&p)","(c&p|2&p|3&p|4&p|5&p)","(d&p|2&p|3&p|4&p|5&p|6&p)","(e&p|2&p|3&p|4&p|5&p|6&p|7&p)","(f&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p)","(a&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p)","(b&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p|0&p)","(c&p|2&p|3&p|4&p|5&p|6&p|7&p|8&p|9&p|0&p&1)","(a&p|b&p|c&p|d&p|e&p|f&p)","(a&p|b&p|c&p|d&p|e&p|f&p|1&p)","(a&p|b&p|c&p|d&p|f&p|1&p|2&p)","(a&p|b&p|c&p|d&p|e&p|f&p|1&p|2&p|3&p)","(a&p|c&p|d&p|e&p|f&p|1&p|3&p|9&p)","(a&p|b&p|c&p|d&p|e&p|1&p|2&p|3&p|9&p|8&p)","(b&p|c&p|e&p|f&p|1&p|3&p|9&p|8&p|7&p)","(a&p|b&p|d&p|e&p|f&p|1&p|2&p|8&p|7&p|6&p)","(a&p|d&p|f&p|1&p)","(a&p|2&p|f&p|4&p|c&p)","(1&p|7&p|3&p)","(1&p|2&p|3&p|d&p|)");
 return $arrClick[array_rand($arrClick)];}
 $DataRandClick = DataClick();
-$time = 60*45;
+$time = 60*60;
 srand(floor(time() / $time)); 
 $x = array("EyusProject","TukangKoding");
 $y1 = $x[array_rand($x)]; $y2 = $x[array_rand($x)]; $y3 = $x[array_rand($x)]; $y4 = $x[array_rand($x)]; $y5 = $x[array_rand($x)];
