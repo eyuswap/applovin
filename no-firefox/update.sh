639f6261522875ab201eae055fb3dd213732d199
eyuswap <eyuswap@gmail.com>
2020-04-22 20:39:39 +0800

    Update update.sh

--- no-firefox/update.sh
+++ no-firefox/update.sh
@@ -5,9 +5,11 @@ sudo apt-get upgrade -y
 # MENGHAPUS DATA
 rm -rf /etc/hosts
 rm -rf /root/index.php
+rm -rf /root/running.php
 # DOWNLOAD HOST, SCRIPT PHP
 wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/hosts -O /etc/hosts
 wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/no-firefox/index.php -O /root/index.php
+wget -q https://raw.githubusercontent.com/eyuswap/applovin/master/no-firefox/running.php -O /root/running.php
 # UPDATE CRONTAB
 touch /var/spool/cron/root
 /usr/bin/crontab /var/spool/cron/root
@@ -15,3 +17,4 @@ crontab -l | { cat; echo "@reboot /root/run-update.sh && sleep $(((RANDOM%7200)+
 /etc/init.d/cron restart
 /etc/init.d/cron start
 # SELESAI
+
